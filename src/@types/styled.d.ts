e86676ce233a7b98209992a6dc2876f80171f335
Felipe Andrade <flpandrade2011@gmail.com>
2023-03-05 08:17:33 -0300

    build: configure styled-components

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components';
+
+import { colors } from '../theme/colors';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: typeof colors;
+  }
+}
