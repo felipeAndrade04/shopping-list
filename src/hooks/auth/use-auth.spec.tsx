5404286de5e72531b84e37203390de19ece88a2c
Felipe Andrade <flpandrade2011@gmail.com>
2023-05-27 17:34:38 -0300

    test: when useAuth to fail

--- src/hooks/auth/use-auth.spec.tsx
+++ src/hooks/auth/use-auth.spec.tsx
@@ -14,17 +14,6 @@ jest.mock('@app/services', () => ({
 }));
 
 describe('useAuth', () => {
-  beforeEach(() => {
-    jest.spyOn(services.auth, 'login').mockImplementation((user: LoginParams) => {
-      return new Promise((resolve) => {
-        resolve({
-          id: '1',
-          name: 'teste',
-          email: user.email,
-        });
-      });
-    });
-  });
   afterEach(() => {
     jest.restoreAllMocks();
     resetStore();
@@ -35,6 +24,15 @@ describe('useAuth', () => {
       email: 'teste@email.com',
       password: '123456',
     };
+    jest.spyOn(services.auth, 'login').mockImplementation((user: LoginParams) => {
+      return new Promise((resolve) => {
+        resolve({
+          id: '1',
+          name: 'teste',
+          email: user.email,
+        });
+      });
+    });
     const { result } = renderHook(() => useAuth(), { wrapper });
 
     await waitFor(() => {
@@ -46,12 +44,43 @@ describe('useAuth', () => {
     expect(result.current.isAuthenticated).toBeTruthy();
   });
 
+  it('Should failed login', async () => {
+    const user = {
+      email: 'teste@email.com',
+      password: '12345678',
+    };
+    jest.spyOn(services.auth, 'login').mockImplementation(() => {
+      return new Promise((_resolve, reject) => {
+        return reject(new Error('Login failed'));
+      });
+    });
+    const { result } = renderHook(() => useAuth(), { wrapper });
+
+    await waitFor(() => {
+      result.current.login(user);
+    });
+
+    expect(services.auth.login).toBeCalled();
+    expect(services.auth.login).rejects.toThrow();
+    expect(result.current.isAuthenticated).toBeFalsy();
+    expect(result.current.user).toBeNull();
+  });
+
   it('Should be register correctly', async () => {
     const user: RegisterParams = {
       name: 'Teste',
       email: 'teste@email.com',
       password: '123456',
     };
+    jest.spyOn(services.auth, 'login').mockImplementation((user: LoginParams) => {
+      return new Promise((resolve) => {
+        resolve({
+          id: '1',
+          name: 'teste',
+          email: user.email,
+        });
+      });
+    });
     const { result } = renderHook(() => useAuth(), { wrapper });
 
     await waitFor(() => {
@@ -62,6 +91,29 @@ describe('useAuth', () => {
     expect(services.auth.login).toBeCalledWith({ email: user.email, password: user.password });
     expect(result.current.user.email).toBe(user.email);
     expect(result.current.isAuthenticated).toBeTruthy();
+  });
+
+  it('Should failed register', async () => {
+    const user = {
+      name: 'Teste',
+      email: 'teste@email.com',
+      password: '12345678',
+    };
+    jest.spyOn(services.auth, 'register').mockImplementation(() => {
+      return new Promise((_resolve, reject) => {
+        return reject(new Error('Register failed'));
+      });
+    });
+    const { result } = renderHook(() => useAuth(), { wrapper });
+
+    await waitFor(() => {
+      result.current.register(user);
+    });
+
+    expect(services.auth.register).toBeCalled();
+    expect(services.auth.register).rejects.toThrow();
+    expect(result.current.isAuthenticated).toBeFalsy();
+    expect(result.current.user).toBeNull();
   });
 
   it('Should be logout correctly', async () => {
@@ -76,6 +128,22 @@ describe('useAuth', () => {
     expect(result.current.isAuthenticated).toBeFalsy();
   });
 
+  it('Should failed logout', async () => {
+    jest.spyOn(services.auth, 'logout').mockImplementation(() => {
+      return new Promise((_resolve, reject) => {
+        return reject(new Error('Logout failed'));
+      });
+    });
+    const { result } = renderHook(() => useAuth(), { wrapper });
+
+    await waitFor(() => {
+      result.current.logout();
+    });
+
+    expect(services.auth.logout).toHaveBeenCalled();
+    expect(services.auth.logout).rejects.toThrow();
+  });
+
   it('Should be forgot password correctly', async () => {
     const email = 'teste@teste.com';
     const { result } = renderHook(() => useAuth(), { wrapper });
@@ -86,5 +154,22 @@ describe('useAuth', () => {
 
     expect(services.auth.forgotPassword).toBeCalledWith(email);
     expect(result.current.isAuthenticated).toBeFalsy();
+  });
+
+  it('Should failed forgot password', async () => {
+    const email = 'teste@email.com';
+    jest.spyOn(services.auth, 'forgotPassword').mockImplementation(() => {
+      return new Promise((_resolve, reject) => {
+        return reject(new Error('Forgot password failed'));
+      });
+    });
+    const { result } = renderHook(() => useAuth(), { wrapper });
+
+    await waitFor(() => {
+      result.current.forgotPassword(email);
+    });
+
+    expect(services.auth.forgotPassword).toBeCalled();
+    expect(services.auth.forgotPassword).rejects.toThrow();
   });
 });
