f8ac467ea147020fff1e46536a7296cc1f28bc3d
Felipe Andrade <flpandrade2011@gmail.com>
2023-08-20 09:40:25 -0300

    test: fix test

--- src/mocks/mockShopping.ts
+++ src/mocks/mockShopping.ts
@@ -3,6 +3,6 @@ import { Shopping } from '@app/models';
 export const mockedShopping: Shopping = {
   products: [],
   name: 'Lista 8',
-  created_at: new Date('01/01/2023'),
+  created_at: new Date('2023-01-01T10:00:00Z'),
   id: '123',
 };
